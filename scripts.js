a6dd101e206df8a907f2fb46c719749711a11565
ivansheshin <ivansheshin@gmail.com>
2021-05-10 18:20:25 +0300

    В разметке ссылки find gift now и gift finder заменены на кнопки, добавлены скрипты для попапов для всех кнопок

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+let modal = document.querySelector('.modal-gifts');
+let findgift = document.querySelectorAll('.btn_popup');
+let modalclose = document.querySelector('.modal-close');
 
 
+findgift.forEach(function(findgift){
+    findgift.addEventListener('click', function(evt){
+        evt.preventDefault();
+        modal.classList.add('modal-show');
+    })
 
+});
+modalclose.addEventListener('click', function(evt){
+    evt.preventDefault();
+    modal.classList.remove('modal-show');
+})
