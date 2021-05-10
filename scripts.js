62e0066ade732862a7642f62b54d137228b44741
ivansheshin <ivansheshin@gmail.com>
2021-05-10 19:05:08 +0300

    Добавлен оверлей, добавлен для него скрипт (появление при открытии модалки, закрытие модалки при клике на оверлей)

--- scripts.js
+++ scripts.js
@@ -1,16 +1,24 @@
 let modal = document.querySelector('.modal-gifts');
 let findgift = document.querySelectorAll('.btn_popup');
 let modalclose = document.querySelector('.modal-close');
-
+let overlay = document.querySelector('.overlay')
 
 findgift.forEach(function(findgift){
     findgift.addEventListener('click', function(evt){
         evt.preventDefault();
         modal.classList.add('modal-show');
+        overlay.classList.add('modal-show');
     })
 
 });
 modalclose.addEventListener('click', function(evt){
     evt.preventDefault();
     modal.classList.remove('modal-show');
+    overlay.classList.remove('modal-show');
 })
+overlay.addEventListener('click', function(evt){
+    evt.preventDefault();
+    modal.classList.remove('modal-show');
+    overlay.classList.remove('modal-show');
+})
+
