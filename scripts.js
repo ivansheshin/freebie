2fb5023edb5cabc0d3ef7029bb477b1e56213284
ivansheshin <ivansheshin@gmail.com>
2021-05-15 10:30:40 +0300

    в функции открытия модалки в методе класслист классы объединены по блокам; переработана функция анимации закрытия с последующим вызовом функции и назначения аргументов; в функции закрытия модалки по escape изменен код клавиши на 'escape', сгруппированы классы в методе класслист

--- scripts.js
+++ scripts.js
@@ -1,61 +1,52 @@
-let modal = document.querySelector('.modal-gifts');
-let findgift = document.querySelectorAll('.btn_popup');
-let modalСlose = document.querySelector('.modal-close');
-let overlay = document.querySelector('.overlay')
+ // анимация открытия модалки при клике на кнопку
+ const findgift = document.querySelectorAll('.btn_popup');
+ const modal = document.querySelector('.modal-gifts');
+ const overlay = document.querySelector('.overlay');
 
-findgift.forEach(function(findgift){
-    findgift.addEventListener('click', function(evt){
-        evt.preventDefault();
-        modal.classList.add('modal-animation-opening');
-        modal.classList.add('modal_display-block');
-        overlay.classList.add('modal-animation-opening');
-        overlay.classList.add('modal_display-block');
+ findgift.forEach(function (findgift) {
+     findgift.addEventListener('click', function (evt) {
+         evt.preventDefault();
+         modal.classList.add('modal-animation-opening', 'modal_display-block');
+         overlay.classList.add('modal-animation-opening', 'modal_display-block');
 
-    })
+     })
 
-});
-modalСlose.addEventListener('click', function(evt){
-    evt.preventDefault();
-    modal.classList.remove('modal-animation-opening');
-    modal.classList.add('modal-animation-closing');
-    setTimeout(() => {modal.classList.remove('modal-animation-closing')
-        
-    }, 100);
-    setTimeout(() => {modal.classList.remove('modal_display-block')
-        
-    }, 100);
-    overlay.classList.remove('modal_display-block');
-})
-overlay.addEventListener('click', function(evt){
-    evt.preventDefault();
-    modal.classList.remove('modal-animation-opening');
-    modal.classList.add('modal-animation-closing');
-    setTimeout(() => {modal.classList.remove('modal-animation-closing')
-        
-    }, 100);
-    setTimeout(() => {modal.classList.remove('modal_display-block')
-        
-    }, 100);
-    overlay.classList.remove('modal_display-block');
-})
+ });
 
-window.addEventListener('keydown', function(evt){
-    if(evt.keyCode === 27) {
-        if(modal.classList.contains('modal_display-block')){
-            evt.preventDefault();
-            modal.classList.remove('modal-animation-opening');
-            modal.classList.add('modal-animation-closing');
-            setTimeout(() => {modal.classList.remove('modal-animation-closing')
-                
-            }, 100);
-            setTimeout(() => {modal.classList.remove('modal_display-block')
-                
-            }, 100);
-            overlay.classList.remove('modal_display-block');
+ // анимация закрытия модалки
+ const modalСlose = document.querySelector('.modal-close');
 
-        }
-    }
+ function animationModal(closingElement, mainModal, mainOverlay) {
+     closingElement.addEventListener('click', function (evt) {
+         mainModal.classList.remove('modal-animation-opening');
+         mainModal.classList.add('modal-animation-closing');
+         setTimeout(() => {
+             mainModal.classList.remove('modal-animation-closing', 'modal_display-block')
 
-})
+         }, 100);
+         mainOverlay.classList.remove('modal_display-block');
+     })
+ };
+
+ animationModal(modalСlose, modal, overlay);
+ animationModal(overlay, modal, overlay);
 
 
+
+ window.addEventListener('keydown', function (evt) {
+     if (modal.classList.contains('modal_display-block') && evt.key === "Escape") {
+         evt.preventDefault();
+         modal.classList.remove('modal-animation-opening');
+         modal.classList.add('modal-animation-closing');
+         setTimeout(() => {
+             modal.classList.remove('modal-animation-closing', 'modal_display-block')
+
+         }, 100);
+         overlay.classList.remove('modal_display-block');
+
+     }
+
+
+ })
+ // let promtmodal = prompt('Write your name', 'Your name');
+ // alert(`Welcome, ${promtmodal}! How are you?`);
