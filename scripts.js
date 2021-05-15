ef3d11936cc9bfb20b2bdcd09f7e8b7767f95fd8
ivansheshin <ivansheshin@gmail.com>
2021-05-15 20:06:43 +0300

    Переработан джс: создана функция для отслеживания действий на всех блоках

--- scripts.js
+++ scripts.js
@@ -1,52 +1,42 @@
- // анимация открытия модалки при клике на кнопку
- const findgift = document.querySelectorAll('.btn_popup');
- const modal = document.querySelector('.modal-gifts');
- const overlay = document.querySelector('.overlay');
-// анимация открытия модалки
- findgift.forEach(function (findgift) {
-     findgift.addEventListener('click', function (evt) {
-         evt.preventDefault();
-         modal.classList.add('modal-animation-opening', 'modal_display-block');
-         overlay.classList.add('modal-animation-opening', 'modal_display-block');
+function initListeners() {
+    const findgift = document.querySelectorAll('.btn_popup');
+    const modal = document.querySelector('.modal-gifts');
+    const overlay = document.querySelector('.overlay');
 
-     })
+    findgift.forEach(function (findgift) {
+        findgift.addEventListener('click', function (evt) {
+            openModal(modal, overlay)
+        })
 
- });
+    });
+    const modalСloseBtn = document.querySelector('.modal-close');
 
- // анимация закрытия модалки
- const modalСlose = document.querySelector('.modal-close');
+    modalСloseBtn.addEventListener('click', function (evt) {
+        closeModal(modal, overlay)
+    })
+    window.addEventListener('keydown', function (evt) {
+        if (modal.classList.contains('modal_display-block') && evt.key === "Escape") {
+            evt.preventDefault();
+            closeModal(modal, overlay)
 
- function animationModal(closingElement, mainModal, mainOverlay) {
-     closingElement.addEventListener('click', function (evt) {
-         mainModal.classList.remove('modal-animation-opening');
-         mainModal.classList.add('modal-animation-closing');
-         setTimeout(() => {
-             mainModal.classList.remove('modal-animation-closing', 'modal_display-block')
+        }
 
-         }, 100);
-         mainOverlay.classList.remove('modal_display-block');
-     })
- };
+    })
+}
 
- animationModal(modalСlose, modal, overlay);
- animationModal(overlay, modal, overlay);
+function openModal(mainModal, mainOverlay) {
+    mainModal.classList.add('modal-animation-opening', 'modal_display-block');
+    mainOverlay.classList.add('modal-animation-opening', 'modal_display-block');
+}
 
-// закрытие по клавише escape
+function closeModal(mainModal, mainOverlay) {
+    mainModal.classList.remove('modal-animation-opening');
+    mainModal.classList.add('modal-animation-closing');
+    setTimeout(() => {
+        mainModal.classList.remove('modal-animation-closing', 'modal_display-block')
 
- window.addEventListener('keydown', function (evt) {
-     if (modal.classList.contains('modal_display-block') && evt.key === "Escape") {
-         evt.preventDefault();
-         modal.classList.remove('modal-animation-opening');
-         modal.classList.add('modal-animation-closing');
-         setTimeout(() => {
-             modal.classList.remove('modal-animation-closing', 'modal_display-block')
+    }, 100);
+    mainOverlay.classList.remove('modal_display-block');
+};
 
-         }, 100);
-         overlay.classList.remove('modal_display-block');
-
-     }
-
-
- })
- // let promtmodal = prompt('Write your name', 'Your name');
- // alert(`Welcome, ${promtmodal}! How are you?`);
+initListeners()
